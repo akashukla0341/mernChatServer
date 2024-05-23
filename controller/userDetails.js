d9489c362823e1c89ec072e35a9013cad096cabc
Akash Shukla <akashukla0341@gmail.com>
2024-05-23 15:27:14 +0530

    changes

--- controller/userDetails.js
+++ controller/userDetails.js
@@ -2,13 +2,14 @@ const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
 
 async function userDetails(request, response) {
   try {
-    const token = request.cookies.token || localStorage.getItem('token') ;
+    const token = request.cookies.token || "";
 
     const user = await getUserDetailsFromToken(token);
 
     return response.status(200).json({
       message: "user details",
       data: user,
+      token:token
     });
   } catch (error) {
     return response.status(500).json({
